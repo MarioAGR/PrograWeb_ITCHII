88a0451a1cfe278946ad3d90b584bae5f274b714
Mario Guillen <marioagr95@outlook.com>
2017-05-01 12:19:23 -0600

    Fixed AngularJS index.html and app.js, added Bootstrap work

--- Progra_web/Angular/js/app.js
+++ Progra_web/Angular/js/app.js
@@ -12,41 +12,58 @@
         soldOut: true
     }
 
-    var gems = [
-        {
-            name: 'Safiro',
-            price: 1,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            images: [
-                {
-                    full: './public/img/img-01.png',
-                    thumb: './public/img/img-04.png'
-                }
-            ]
-        },
-        {
-            name: 'Ruby',
-            price: 10,
-            description: 'Nihil molestiae reprehenderit excepturi qui, eum facilis tempora, quaerat cumque, necessitatibus autem doloribus modi amet, asperiores!',
-            images: [
-                {
-                    full: './public/img/img-02.png',
-                    thumb: './public/img/img-05.png'
-                }
-            ]
-        },
-        {
-            name: 'Esmeralda',
-            price: 100,
-            description: 'Deserunt ducimus molestiae, eius fugit asperiores.',
-            images: [
-                {
-                    full: './public/img/img-03.png',
-                    thumb: './public/img/img-06.png'
-                }
-            ]
-        }
-    ]
+    var gems = [{
+        name: 'Safiro',
+        price: 1,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        images: [{
+            full: './public/img/img-01.png',
+            thumb: './public/img/img-04.png'
+        }],
+        reviews: [{
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com"
+        }, {
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com"
+        }]
+    }, {
+        name: 'Ruby',
+        price: 10,
+        description: 'Nihil molestiae reprehenderit excepturi qui, eum facilis tempora, quaerat cumque, necessitatibus autem doloribus modi amet, asperiores!',
+        images: [{
+            full: './public/img/img-02.png',
+            thumb: './public/img/img-05.png'
+        }],
+        reviews: [{
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com"
+        }, {
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com"
+        }]
+    }, {
+        name: 'Esmeralda',
+        price: 100,
+        description: 'Deserunt ducimus molestiae, eius fugit asperiores.',
+        images: [{
+            full: './public/img/img-03.png',
+            thumb: './public/img/img-06.png'
+        }],
+        reviews: [{
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com",
+        }, {
+            stars: 3,
+            body: "asdasdad",
+            author: "das@adasd.com"
+        }]
+    }]
 
     app.controller('StoreController', function() {
         this.products = gems;
@@ -64,6 +81,15 @@
             /*Con 3 iguales siempre se compara el elemtento*/
         }
 
+    });
+
+    app.controller('ReviewController', function() {
+        this.review = {};
+
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        }
     });
 
 })();
